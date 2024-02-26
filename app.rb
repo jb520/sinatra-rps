ab556430115663d6791e0128eedd020b81ea0e32
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-26 23:09:35 +0000

    add rock, paper, scissors

--- app.rb
+++ app.rb
@@ -3,5 +3,26 @@ require 'sinatra'
 get('/') do
 
   erb(:rules)
-  
+
+end
+
+get('/rock') do
+  @player_choice = "rock"
+  comp_choices = ["rock","paper","scissors"]
+  @computer_choice = comp_choices.sample
+  erb(:rock)
+end
+
+get('/paper') do
+  @player_choice = "paper"
+  comp_choices = ["rock","paper","scissors"]
+  @computer_choice = comp_choices.sample
+  erb(:paper)
+end
+
+get('/scissors') do
+  @player_choice = "scissors"
+  comp_choices = ["rock","paper","scissors"]
+  @computer_choice = comp_choices.sample
+  erb(:scissors)
 end
